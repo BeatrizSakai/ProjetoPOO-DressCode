49ded014273607ddbf5a91db8eb3a5f4749a89bb
Beatriz Sakai <biasakai@outlook.com>
2023-06-21 13:58:28 -0300

    Adiciona classe Vestido

---
+++ scr/Vestido.java
@@ -0,0 +1,37 @@
+package scr;
+public class Vestido extends Roupa{
+    private String comprimento;
+    private boolean manga;
+    private String tipo_decote; 
+
+    public Vestido(String nome,String cor, double preco,String tamanho, String comprimento, boolean manga, String tipo_decote){
+        super(nome,cor, preco, tamanho);
+        this.comprimento = comprimento;
+        this.manga = manga;
+        this.tipo_decote = tipo_decote;
+    }
+
+    public String getComprimento(){
+        return comprimento;
+    }
+
+    public void setComprimento(String comprimento){
+        this.comprimento = comprimento;
+    }
+
+    public boolean hasManga(){
+        return manga;
+    }
+
+    public void setManga(boolean manga){
+        this.manga = manga;
+    }
+
+    public String getTipo_decote(){
+        return tipo_decote;
+    }
+
+    public void setTipo_decote(String tipo_decote){
+        this.tipo_decote = tipo_decote;
+    }
+}
