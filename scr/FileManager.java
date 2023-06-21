fb1744ee9e7c5e0358d9adc661ec59e001db46a3
Beatriz Sakai <biasakai@outlook.com>
2023-06-21 20:23:04 -0300

    altera o FileManager

--- scr/FileManager.java
+++ scr/FileManager.java
@@ -6,16 +6,12 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
+
 
 
 public class FileManager {
   private String caminhoArquivo;
-  private Random random = new Random();
-  String[][] lista = {
-    {"8", "Scouller" + random.nextInt(10)},
-    {"9", "Jagoe" + random.nextInt(10),}
-  };
+  List<Object> lista = Pedido.finalizarPedido();
 
   public FileManager(String caminhoArquivo) {
     this.caminhoArquivo = caminhoArquivo;
@@ -37,26 +33,12 @@ public class FileManager {
       e.printStackTrace();
     }
   }
-
   public void escreveArquivo() {
     File arquivo = new File(caminhoArquivo);
     try {
       FileWriter fileWriter = new FileWriter(arquivo, true);
-      for (String[] valor : lista) {
-        StringBuilder line = new StringBuilder();
-        for (int i = 0; i < valor.length; i++) {
-          line.append(valor[i]);
-          if (i != valor.length - 1) {
-            line.append(',');
-          }
-        }
-        line.append("\n");
-        fileWriter.append(line.toString());
-      }
-      fileWriter.close();
-    } catch (Exception e) {
-      e.printStackTrace();
     }
-  }
 }
+}
+
 
